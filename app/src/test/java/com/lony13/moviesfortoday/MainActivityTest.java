75c96e0ab004a4dfc5fe865bb47e21796eb85796
Wojciech Koczy <wojteklony@gmail.com>
2018-02-07 12:27:52 +0100

    Initial commit

---
+++ app/src/test/java/com/lony13/moviesfortoday/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.lony13.moviesfortoday;
+
+/**
+ * Created by Lony13 on 2018-01-27.
+ */
+
+public class MainActivityTest {
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule =
+//            new ActivityTestRule<>(MainActivity.class);
+//
+//    @Test
+//    public void compareTimeTest(){
+//        Assert.assertTrue(mainActivity.compareTime("9:25", 8, 20));
+//        Assert.assertTrue();
+//    }
+}
